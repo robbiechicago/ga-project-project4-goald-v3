1012041a47aebcbe1b23374ff768b613e428135c
Rob Holloway <robbiechicago@gmail.com>
2015-11-12 15:22:58 +0000

    created year, month, week and day arrays

--- frontend/js/usereventsCtrl.js
+++ frontend/js/usereventsCtrl.js
@@ -7,8 +7,6 @@ function userEventsCtrl($http, $state, $stateParams) {
 
   var self = this;
   self.all = [];
-  self.datetimes = [];
-  self.hours = [];
 
   console.log('$stateParams.user_id = ' + $stateParams.user_id)
   console.log('$stateParams.goal_id = ' + $stateParams.goal_id)
@@ -31,26 +29,69 @@ function userEventsCtrl($http, $state, $stateParams) {
     });
   }
 
+  self.datetimes = [];
+
   self.getDatetimess = getDatetimess;
   function getDatetimess() {
     self.datetimes = self.all.map(function(a) {
-      return a.event_datetime;
+      return new Date(a.event_datetime);
     });
     console.log(self.datetimes);
-    eventHours();
+    eventDateElements();
   }
 
-  self.eventHours = eventHours
-  function eventHours() {
+  self.hours = [];
+  self.months = [];
+  self.years = [];
+  self.days = [];
+  self.weeks = [];
+
+  self.eventDateElements = eventDateElements
+  function eventDateElements() {
     for (var i = 0; i < self.datetimes.length; i++) {
-      self.theDate = new Date(self.datetimes[i]);
+      // self.theDate = new Date(self.datetimes[i]);
+      self.theDate = self.datetimes[i];
       self.hour = self.theDate.getHours();
+      self.day = self.theDate.getDay();
+      self.week = moment(self.theDate).isoWeek()
+      self.month = self.theDate.getMonth() + 1;  // MONTHS ARE ZERO INDEXED.  WTF?!
+      self.year = self.theDate.getFullYear();
       if (self.hours.indexOf(self.hour) === -1) {
         self.hours.push(self.hour);
       }
+      if (self.days.indexOf(self.day) === -1) {
+        self.days.push(self.day);
+      }
+      if (self.weeks.indexOf(self.week) === -1) {
+        self.weeks.push(self.week);
+      }
+      if (self.months.indexOf(self.month) === -1) {
+        self.months.push(self.month);
+      }
+      if (self.years.indexOf(self.year) === -1) {
+        self.years.push(self.year);
+      }
     }
     self.hours.sort()
+    self.days.sort()
+    self.weeks.sort()
+    self.months.sort()
+    self.years.sort()
     console.log('Hours: ' + self.hours)
+    console.log('Days: ' + self.days)
+    console.log('Weeks: ' + self.weeks)
+    console.log('Months: ' + self.months)
+    console.log('Years: ' + self.years)
+  }
+
+  self.minDate = minDate
+  function minDate() {
+
+  }
+
+  self.dateElementTotals = dateElementTotals
+  function dateElementTotals() {
+
   }
 
 }
